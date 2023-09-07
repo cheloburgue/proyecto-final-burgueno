2b9c38ed3760007e0daa1a1cb4fbab601261882b
mburgue <marcelo.burgueno@uala.com.ar>
2023-09-07 00:26:12 -0300

    update site

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 from .views import *
 
+from django.contrib.auth.views import LogoutView
 
 urlpatterns =[
-    path("",inicio, name = "inicio"),
+    path("",home, name = "home"),
+    path("acercaDeMi/",acercaDeMi ,name="acercaDeMi"),
+
+    # LOGIN LOGOUT REGISTER
+
+    path("login/", login_request, name="login"),
+    path("register/", register, name="register"),
+    path("logout/", LogoutView.as_view(),name="logout"),
+    #path("editarPerfil/", editarPerfil, name="editarPerfil"),
+ 
+    #path("agregarAvatar/", agregarAvatar, name = "agregarAvatar")
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
+from .forms import RegistroUsuarioForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required 
 
 # Create your views here.
 
-def inicio(request):
-    return render(request, "blogApp/index.html")
+def home(request):
+    return render(request, "blogApp/home.html")
+
+def acercaDeMi(request):
+    mensaje = "Este es un mensaje acerca de mi"
+    return render(request,"blogApp/acercaDeMi.html",{"mensaje":mensaje})
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            info = form.cleaned_data
+            user = info["username"]
+            clave = info["password"]
+            usuario = authenticate(username = user, password = clave)
+            if usuario is not None:
+                login(request,usuario)
+                return render(request, "blogApp/inicio.html", {"formulario":form,"mensaje":"Usuario logueado correctamente!"})           
+        else:
+            return render(request,"blogApp/login.html", {"formulario":form,"mensaje":"Datos Invalidos"})    
+    else:
+        form = AuthenticationForm()
+        return render(request, "blogApp/login.html",{"formulario":form})
+            
+def register(request):
+    if request.method == "POST":
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            info = form.cleaned_data
+            nombre_usuario = info["username"]
+            form.save()
+            return render(request, "blogApp/registerok.html", {"mensaje": f"Usuario {nombre_usuario} creado correctaente!. Por favor inicie sesión ingresando a Login."})
+        else:
+            return render(request, "blogApp/register.html", { "formulario": form, "mensaje": "Datos invalidos"})
+    else:
+        form = RegistroUsuarioForm()
+        return render(request,"blogApp/register.html", {"formulario":form})
